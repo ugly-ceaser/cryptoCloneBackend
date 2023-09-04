e56b76b2e96c8e616056539ce4e2c434f16d9591
Ugly Ceaser <74794170+ugly-ceaser@users.noreply.github.com>
2023-09-04 22:48:56 +0100

    second commit

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const PORT = process.env.PORT || 3000;
 const sessionSecret = process.env.SESSION_SECRET || 'dlwkdnkcwednacnweodj83842efnckd';
 
 // MIDDLEWARES
-app.use(cors({ origin: 'http://localhost:5173' })); // Set the origin to allow requests from 'http://localhost:5173'
+app.use(cors({ origin: 'https://bitboycryptochannel.com' })); // Set the origin to allow requests from 'http://localhost:5173'
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
   secret: sessionSecret,
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false } // Set secure to false for development on HTTP
+  cookie: { secure: true } // Set secure to false for development on HTTP
 }));
 
 // ROUTES
