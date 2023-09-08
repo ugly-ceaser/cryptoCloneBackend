1b42124d7d2ebb940e7dd31d6c53abbbc5860864
Ugly Ceaser <74794170+ugly-ceaser@users.noreply.github.com>
2023-09-09 00:16:06 +0100

    commiting for cors again

--- server.js
+++ server.js
@@ -2,14 +2,29 @@ const express = require('express');
 const multer = require('multer');
 const dbConnect = require('./controller/Connect');
 const session = require('express-session');
-const cors = require('cors');
+const cors = require('cors'); // Added the cors middleware
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const sessionSecret = process.env.SESSION_SECRET || 'dlwkdnkcwednacnweodj83842efnckd';
 
 // MIDDLEWARES
-app.use(cors()); // Enable CORS for all origins
+
+
+const allowedOrigins = [
+  'https://bitboycryptochannel.com',
+  'http://localhost:5173',
+];
+
+const corsOptions = {
+  origin: allowedOrigins, // Replace with your frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allow cookies and sessions
+};
+
+
+// Use CORS middleware with the allowed origins
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
@@ -24,5 +39,5 @@ const allRoutes = require('./routes');
 app.use('/api/v1', allRoutes);
 
 dbConnect(() => {
-  app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
+  app.listen(PORT, () => console.log(`app is listening on port ${PORT}`));
 });
