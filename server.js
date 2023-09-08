9b137c88cbc356bd5f280f4637373815818dd0fe
Ugly Ceaser <74794170+ugly-ceaser@users.noreply.github.com>
2023-09-09 00:04:18 +0100

    committing for cors

--- server.js
+++ server.js
@@ -13,18 +13,12 @@ const sessionSecret = process.env.SESSION_SECRET || 'dlwkdnkcwednacnweodj83842ef
 
 const allowedOrigins = [
   'https://bitboycryptochannel.com',
-  'http://localhost:5173',
+  'http://localhost:5173'
+
 ];
 
-const corsOptions = {
-  origin: allowedOrigins, // Replace with your frontend URL
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // Allow cookies and sessions
-};
-
-
 // Use CORS middleware with the allowed origins
-app.use(cors(corsOptions));
+app.use(cors({ origin: allowedOrigins }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
